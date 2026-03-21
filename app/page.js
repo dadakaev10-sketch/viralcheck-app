42cbcbd7570a3c2caa0ef4299206945ef5db76a1
Amok <dadakaev10@gmail.com>
2026-03-22 02:44:42 +0700

    Fix JSX syntax: add missing closing div after Stepper

--- app/page.js
+++ app/page.js
@@ -1003,6 +1003,7 @@ export default function Home() {
                         </div>
                       </Step>
                     </Stepper>
+                  </div>
 
               </>
             )}
